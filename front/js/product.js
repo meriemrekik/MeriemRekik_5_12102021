236312630698baf6d79a31ba8208bb4decfd0603
Meriem Rekik <rekik.meriem@gmail.com>
2021-10-19 22:29:03 +0200

    afficher liste des canapes et afficher canape dans la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,30 +1,63 @@
-let myProduct = {
-    "colors": [
-        "Green",
-        "Red",
-        "Orange"
-    ],
-    "_id": "055743915a544fde83cfdfc904935ee7",
-    "name": "Kanap Calycé",
-    "price": 3199,
-    "imageUrl": "http://localhost:3000/images/kanap03.jpeg",
-    "description": "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
-    "altTxt": "Photo d'un canapé d'angle, vert, trois places"
-};
+const urlApi = "http://localhost:3000/api";
 
-let priceElement = document.getElementById('price');
-priceElement.innerText = myProduct['price'];
 
-let titleElement = document.getElementById('title');
-titleElement.innerText = myProduct['name'];
+let getIdFromUrl = () => {
+    let url = new URL(document.location.href);
+    let search_params = new URLSearchParams(url.search);
+    if (search_params.has('id')) {
+        let id = search_params.get('id');
+        console.log(id);
+        return id;
+    }
+    return null;
+}
 
-let descriptionElement = document.getElementById('description');
-descriptionElement.innerText = myProduct['description'];
+const id = getIdFromUrl();
 
-let imageContainerElement = document.querySelector("section.item article .item__img");
-console.log(imageContainerElement);
-let imageElement = document.createElement("img");
-imageElement.src = myProduct['imageUrl'];
-imageElement.alt = myProduct['altTxt'];
-console.log(imageElement);
-imageContainerElement.appendChild(imageElement);
+let getCanape = (idCanape) => {
+    fetch(urlApi + "/products/" + id)
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function (canape) {
+            afficheCanape(canape);
+        })
+        .catch(function (err) {
+            // Une erreur est survenue
+        });
+
+}
+
+getCanape(id);
+
+let afficheCanape = (myProduct) => {
+
+    let priceElement = document.getElementById('price');
+    priceElement.innerText = myProduct['price'];
+
+    let titleElement = document.getElementById('title');
+    titleElement.innerText = myProduct['name'];
+
+    let descriptionElement = document.getElementById('description');
+    descriptionElement.innerText = myProduct['description'];
+
+
+    let imageContainerElement = document.querySelector("section.item article .item__img");
+    let imageElement = document.createElement("img");
+    imageElement.src = myProduct['imageUrl'];
+    imageElement.alt = myProduct['altTxt'];
+    imageContainerElement.appendChild(imageElement);
+
+
+    let selectElement = document.getElementById("colors");
+    for (let color of myProduct['colors']) {
+        let optionElement = document.createElement("option");
+        optionElement.value = color.toLowerCase();
+        optionElement.innerText = color;
+        selectElement.appendChild(optionElement);
+    }
+
+}
+
---
+++ front/js/script.js
@@ -0,0 +1,72 @@
+
+//recuperer tt les donnée du canapé depuis l'api
+// import * as config from "./config";
+const urlApi = "http://localhost:3000/api";
+let getAllCapane = () => {
+    fetch(urlApi + "/products/")
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function (allCanapes) {
+            displayAllCanape(allCanapes);
+        })
+        .catch(function (err) {
+            // Une erreur est survenue
+        });
+
+}
+getAllCapane();
+
+
+//afficher les canape dans ma page 
+let displayAllCanape = (allCanapes) => {
+
+    //recuperer l'element html section avec id "items"
+    let itemsContainerElement = document.getElementById('items');
+    console.log(itemsContainerElement);
+    for (let canape of allCanapes) {
+        console.log(canape);
+        //je crée ma balise a 
+        let canapeElement = document.createElement("a");
+        //je mes le lien vers la page product avec id 
+        canapeElement.href = "./product.html?id=" + canape._id;
+        //je rajoute ma balise a a ma section id="items"
+        itemsContainerElement.appendChild(canapeElement);
+
+        //je crée ma balise article 
+        let articleElement = document.createElement("article");
+        canapeElement.appendChild(articleElement);
+
+        let imageElement = document.createElement("img");
+        imageElement.src = canape.imageUrl;
+        imageElement.alt = canape.altTxt;
+        articleElement.appendChild(imageElement);
+
+        let titreElement = document.createElement("h3");
+        titreElement.innerText = canape.name;
+        titreElement.className = 'productName';
+        articleElement.appendChild(titreElement);
+
+        let paragrapheElement = document.createElement("p");
+        paragrapheElement.className = 'productDescription';
+        paragrapheElement.innerText = canape.description;
+        articleElement.appendChild(paragrapheElement);
+
+
+
+
+
+
+    }
+}
+
+
+
+//boucler sur les donners de tableau du canapé et pour chaque itération 
+
+
+
+
+
